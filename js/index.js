c040a714fd2a79e00f2774b9e22d3cf5cc44d727
Natalia Camello <nataliaspindola@live.com>
2020-12-05 12:23:01 -0300

    creating work info

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ var name = 'Natália Camello';
 var nameHTML = document.getElementById('nameHTML');
 var texto1 = document.getElementById('texto-1');
 var texto2 = document.getElementById('texto-2');
+var texto3 = document.getElementById('texto-3');
+var copyright = document.getElementById('copyright');
 
 // function sayHi(){
 //     alert('Olá, ' + name + '!');
@@ -19,10 +21,20 @@ function clickProject(){
     // console.log("Clicou no botão Projetos!");
     texto2.style.display = "block";
     texto1.style.display = "none";
+    texto3.style.display = "none";
+    copyright.style.margin = "10px";
 }
 
 function clickAbout(){
     // console.log("Clicou no botão Sobre!");
     texto1.style.display = "block";
     texto2.style.display = "none";
+    texto3.style.display = "none";
+}
+
+function clickWork(){
+    // console.log("Clicou no botão Sobre!");
+    texto1.style.display = "none";
+    texto2.style.display = "none";
+    texto3.style.display = "block";
 }
